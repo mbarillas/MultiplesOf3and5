d98c45386166cf01a2840ab3b7265e788be641d3
Michael Barillas <michaelbarillas91@gmail.com>
2015-07-02 21:34:36 -0700

    Completed Multiples of 3 and 5
    No problems :D

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,31 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Multiples of 3 and 5");
+		Main main = new Main();
+		System.out.println(main.findMultipleOf(0, 10));
+		System.out.println(main.findMultipleOf(0, 1000));
+		System.out.println("End of Multiples of 3 and 5");
 	}
 	
+	/**
+	 * Problem Description
+	 * If we list all the natural numbers below 10 that are 
+	 * multiples of 3 or 5, we get 3, 5, 6 and 9.The sum of 
+	 * these multiples is 23.
+	 * Find the sum of all the multiples of 3 or 5 below 1000.
+	 */
 	
-	
+	public int findMultipleOf(int startRange, int endRange){
+		int sum = 0;
+		
+		for(int i = startRange; i<endRange; i++){
+			if((i % 3) == 0){
+				sum = sum + i;
+			}else if((i % 5) == 0){
+				sum = sum + i;
+			}
+		}
+		
+		return sum;
+	}
 }
